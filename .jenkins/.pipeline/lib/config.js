290147cddd6543670b007a58b49bfd8c1553753d
David Kerins <dkerins@bitbox.ca>
2019-07-10 16:58:20 -0700

    Re-ran yo bcdk:jenkins to include 'build' env

--- .jenkins/.pipeline/lib/config.js
+++ .jenkins/.pipeline/lib/config.js
@@ -5,6 +5,7 @@ const version = '1.0.0'
 const name = 'jenkins'
 
 const phases = {
+  build: {namespace:'gtncer-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
   dev: {namespace:'gtncer-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
   prod: {namespace:'gtncer-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
 };
